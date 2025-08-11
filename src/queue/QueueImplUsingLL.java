489f8a2d03bca24112001457f9c48f7a6978f8fe
jadhAv2808 <pbj8007@gmail.com>
2025-08-11 21:39:47 +0530

    queue started

---
+++ src/queue/QueueImplUsingLL.java
@@ -0,0 +1,27 @@
+package queue;
+
+import linkedList.Node;
+
+public class QueueImplUsingLL {
+
+	public static void main(String[] args) {
+		
+	}
+}
+
+
+class LLQueue{
+	Node front;
+	Node rear;
+	
+	public LLQueue() {
+		front=rear=null;
+	}
+	
+	void enque(int x) {
+		Node node = new Node(x);
+		if(front==null) {
+			
+		}
+	}
+}
